8552e76b5acc874cea82e1e12b5645ab939b33a9
Viktoria <viktrols@yandex.ru>
2021-09-29 21:05:02 +0300

    fix ingredient filters

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -4,7 +4,7 @@ from .models import Ingredient, Recipe
 
 
 class IngredientsFilter(filters.FilterSet):
-    name = filters.CharFilter(field_name='name', lookup_expr='icontains')
+    name = filters.CharFilter(field_name='name', lookup_expr='istartswith')
 
     class Meta:
         model = Ingredient
